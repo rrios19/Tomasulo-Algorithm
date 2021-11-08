4d8f511550c59e5fdb51539540250182c5db92a5
pAuLocordero222 <88518574+pAuLocordero222@users.noreply.github.com>
2021-11-08 00:47:01 -0600

    Creo que esta listo

--- Tomasulo.py
+++ Tomasulo.py
@@ -180,13 +180,12 @@ while True:
                     p.Value1M= j.Value1+j.Value2
                     p.Valid1M=1
                     p.Tag1M=" ~~ "
-                    check = True
 
                 if p.Tag2M == j.Nombre:
                     p.Value2M= j.Value1+j.Value2
                     p.Valid2M=1
                     p.Tag2M=" ~~ "
-                    check = True
+                   
             #se vacia estacion de reserva de la instruccion que se escribio
             j.Valid1=' '
             j.Tag1=" ~~ "
@@ -244,13 +243,13 @@ while True:
                     p.Value1= j.Value1M*j.Value2M
                     p.Valid1=1
                     p.Tag1=" ~~ "
-                    check = True
+                    
                     
                 if p.Tag2 == j.NombreM:
                     p.Value2= j.Value1M*j.Value2M
                     p.Valid2=1
                     p.Tag2=" ~~ "
-                    check = True
+                    
             #se vacia estacion de reserva de la instruccion que se escribio
             j.Valid1M=' '
             j.Tag1M=" ~~ "
@@ -259,103 +258,7 @@ while True:
             j.Tag2M=" ~~ "
             j.Value2M='  '
             j.contCiclosM=0
-    #-------Para multiplicacion            
-    for j in EjecutarMul:
-        if (j.Valid1M==1 and j.Valid2M==1):
-            if j.contCiclosM==6:
-                for k in RAT:
-                    if j.NombreM==k.Tag:
-                        for m in MulRS:
-                            if m.Tag1M == k.Tag:
-                                m.Value1M= j.Value1M*j.Value2M
-                                m.Valid1M=1
-                                m.Tag1M=" ~~ "
-                            if m.Tag2M == k.Tag:
-                                m.Value2M= j.Value1M*j.Value2M
-                                m.Valid2M=1
-                                m.Tag2M=" ~~ "
-
-                        for m in AdderRS:
-                            if m.Tag1 == k.Tag:
-                                m.Value1= j.Value1M*j.Value2M
-                                m.Valid1=1
-                                m.Tag1=" ~~ "
-                            if m.Tag2 == k.Tag:
-                                m.Value2= j.Value1M*j.Value2M
-                                m.Valid2=1
-                                m.Tag2=" ~~ "
-                        k.Valid=1       
-                        k.Tag='..'
-                        k.Value=j.Value1M*j.Value2M
-                        #Se vacia estacion de reserva
-                        j.Valid1M=' '
-                        j.Tag1M=" ~~ "
-                        j.Value1M='  '
-                        j.Valid2M=' '
-                        j.Tag2M=" ~~ "
-                        j.Value2M='  '
-                        j.contCiclosM=0
-                for p in MulRS:
-                    igual = False
-                    if p.Tag1M==Tag2M:
-                        igual=True
-                        
-                    if p.Tag1M == j.NombreM:
-                        p.Value1M= j.Value1M*j.Value2M
-                        p.Valid1M=1
-                        p.Tag1M=" ~~ "
-                        if not igual:
-                            j.Valid1M=' '
-                            j.Tag1M=" ~~ "
-                            j.Value1M='  '
-                            j.Valid2M=' '
-                            j.Tag2M=" ~~ "
-                            j.Value2M='  '
-                            j.contCiclosM=0
-                    if p.Tag2M == j.NombreM:
-                        p.Value2M= j.Value1M*j.Value2M
-                        p.Valid2M=1
-                        p.Tag2M=" ~~ "
-
-                        j.Valid1M=' '
-                        j.Tag1M=" ~~ "
-                        j.Value1M='  '
-                        j.Valid2M=' '
-                        j.Tag2M=" ~~ "
-                        j.Value2M='  '
-                        j.contCiclosM=0
-
-                for p in AdderRS:
-                    igual = False
-                    if p.Tag1==p.Tag2:
-                        igual= True
-                        
-                    if p.Tag1 == j.NombreM:
-                        p.Value1= j.Value1M*j.Value2M
-                        p.Valid1=1
-                        p.Tag1=" ~~ "
-                        
-                        if not igual:
-                            j.Valid1M=' '
-                            j.Tag1M=" ~~ "
-                            j.Value1M='  '
-                            j.Valid2M=' '
-                            j.Tag2M=" ~~ "
-                            j.Value2M='  '
-                            j.contCiclosM=0
-                    if p.Tag2 == j.NombreM:
-                        p.Value2= j.Value1M*j.Value2M
-                        p.Valid2=1
-                        p.Tag2=" ~~ "
-
-                        j.Valid1M=' '
-                        j.Tag1M=" ~~ "
-                        j.Value1M='  '
-                        j.Valid2M=' '
-                        j.Tag2M=" ~~ "
-                        j.Value2M='  '
-                        j.contCiclosM=0
-
+  
     for v in AdderRS:
         if (v.Valid1==1 and v.Valid2==1):
             v.contCiclos+=1
